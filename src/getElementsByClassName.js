a4e547e8bde647387e918eb47ff0c261958b5591
Alexandria Perry <alexandriaperry@alexandria-perrys-macbook.local>
2018-11-06 13:49:43 -0800

    stringify and getElements complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+
+var getElementsByClassName = function(className){
+  var results = [];
+  //Push all nodes that match the condition
+  var nodeChecker = function(node) {
+    
+    if (node.classList && node.classList.contains(className)) {
+      results.push(node);
+    }
+    if (node.childNodes) {
+      node.childNodes.forEach(function(child) {
+        nodeChecker(child);
+      })
+    }
+  }
+    //Check each node to see if there are any childNodes
+      //If the node has a classList equal to className
+        //Push to nodes
+      //If it doesn't loop over each childNode and run the nodeChecker on it. 
+  nodeChecker(document.body);
+  return results;
+}
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,43 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // if (typeof obj === undefined || typeof obj === 'function') {
+  //   return '{}';
+  // }
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      var str = '['
+      for (var i = 0; i < obj.length; i++) {
+        str += stringifyJSON(obj[i]) + ',';
+      }
+        str = str.slice(0, str.length-1) + ']';
+        return str;
+    }
+  } 
+  
+  if (typeof obj === 'object') {
+      if (obj === null) {
+        return 'null';
+      }
+      if(Object.keys(obj).length === 0) {
+        return '{}';
+      }
+      var resultObj = '{'
+        for (var key in obj) {
+          if (typeof obj[key] === 'function') {
+            return '{}'
+          }
+            resultObj += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+          }
+            resultObj = resultObj.slice(0, resultObj.length-1) + '}';
+            return resultObj;
+        } 
+  return '' + obj;
 };
+
+
